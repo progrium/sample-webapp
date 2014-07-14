3f8c04a184cbe264d33dd6d07bd3257b9a933aef
Jeff Lindsay <progrium@gmail.com>
2014-07-14 18:25:10 -0500

    newline

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     return "\n".join([
     	'<div style="text-align: center; font-size: 128px;">Hello World!</div>',
-    	'<div style="text-align: center; font-size: 64px;">{0}</div>'.format(socket.gethostname())])
+    	'<div style="text-align: center; font-size: 64px;">{0}</div>\n'.format(socket.gethostname())])
 
 @app.route("/counter")
 def counter():
@@ -17,7 +17,7 @@ def counter():
 		count = "redis not found"
 	return "\n".join([
     	'<div style="text-align: center; font-size: 128px;">{0}</div>'.format(count),
-    	'<div style="text-align: center; font-size: 64px;">{0}</div>'.format(socket.gethostname())])	
+    	'<div style="text-align: center; font-size: 64px;">{0}</div>\n'.format(socket.gethostname())])	
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
